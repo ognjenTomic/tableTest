e00c44f5a62c9846f819ed2472f3c204acdd5025
budjoskop <gtomicognjen@gmail.com>
2020-02-28 15:48:01 +0100

    first commit

--- tableViewHeader/ViewController.swift
+++ tableViewHeader/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableOutlet: UITableView!
+    
+    let search = UISearchController(searchResultsController: nil)
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UITableViewCell
+        
+        
+        return cell
+
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableOutlet.tableHeaderView = self.search.searchBar
     }
 
 
